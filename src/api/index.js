a9d8060ddcae6518d9b6a0ecb1e9e8d67fb4c17d
bloodcv <554365000@qq.com>
2020-05-19 18:06:46 +0800

    [saya]202005191806

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,8 @@
  * @Version: 1.0
  * @Autor: saya
  * @Date: 2020-04-28 19:54:51
- * @LastEditors: saya
- * @LastEditTime: 2020-05-19 11:03:44
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-05-19 17:20:41
  */
 
 /**
@@ -12,12 +12,27 @@
  * 每个函数返回promise
  */
 
-import ajax from './ajax'
-import jsonp from 'jsonp';
+import ajax from "./ajax";
+import jsonp from "jsonp";
+
+import { message } from "antd";
 
 // 登录
-export const reqLogin = (params) => ajax('/login', params, 'POST');
+export const reqLogin = params => ajax("/login", params, "POST");
 
 /**
  * jsonp请求的接口请求函数
  */
+export const reqWeather = city => {
+  const url = `https://restapi.amap.com/v3/weather/weatherInfo?city=${city}&key=d325c2029eb25fb18127449297f12cfb`;
+  return new Promise((resolve, reject) => {
+    jsonp(url, {}, (err, data) => {
+      if (!err && data.info === "OK") {
+        const { city, weather } = data.lives[0];
+        resolve({ city, weather });
+      } else {
+        message.error('天气请求有误：' + err);
+      }
+    });
+  });
+};
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,24 +1,43 @@
 import React, { Component } from "react";
 
 import "./index.less";
-import weather from './imgs/weather.png';
+import weatherPng from "./imgs/weather.png";
+import { getDateAllStr } from "../../utils/dateUtils";
 
 export default class Header extends Component {
+  state = {
+    city: "",
+    weather: "",
+    currentTime: "",
+  };
+
+  getWeather = () => {
+    setInterval(() => {
+      this.setState({ currentTime: getDateAllStr(Date.now()) });
+    }, 1000);
+  };
+
+  componentDidMount() {
+    this.getWeather();
+  }
+
   render() {
+    const { city, weather, currentTime } = this.state;
+
     return (
       <header>
         <div className='header-top'>
           <span>欢迎，admin</span>
-          <a href="#!">退出</a>
+          <a href='#!'>退出</a>
         </div>
         <div className='header-bottom'>
-          <div className="header-bottom-left">
-            首页
-          </div>
-          <div className="header-bottom-right">
-            <span>2020-05-12 13:38:56</span>
-            <img src={weather} alt="晴"/>
-            <span>晴</span>
+          <div className='header-bottom-left'>首页</div>
+          <div className='header-bottom-right'>
+            <span>{currentTime}</span>
+            <img src={weatherPng} alt='晴' />
+            <span>
+              {city}-{weather}
+            </span>
           </div>
         </div>
       </header>
