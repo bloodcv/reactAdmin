dc3a2232c57304d7c3e657d7fafffc439567de6d
bloodcv <554365000@qq.com>
2020-05-25 20:12:11 +0800

    [saya]202005252012

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,51 +1,84 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { message } from "antd";
 
-import './index.less';
-import weatherPng from './imgs/weather.png';
-import { reqWeather } from '../../api/index';
-import menuList from '../../config/menuConfig';
-import { getDateAllStr } from '../../utils/dateUtils';
+import "./index.less";
+import weatherPng from "./imgs/weather.png";
+import { reqWeather } from "../../api/index";
+import menuList from "../../config/menuConfig";
+import { getDateAllStr } from "../../utils/dateUtils";
+import memoryUtils from "../../utils/memoryUtils";
+import storageUtils from "../../utils/storageUtils";
 
 class Header extends Component {
   state = {
-    city: '',
-    weather: '',
-    currentTime: '',
+    city: "",
+    weather: "",
+    currentTime: "",
   };
 
   getCurrentTime = () => {
-    setInterval(() => {
+    this.intervalId = setInterval(() => {
       this.setState({ currentTime: getDateAllStr(Date.now()) });
     }, 1000);
   };
 
   getWeather = async () => {
-    const { city, weather } = await reqWeather('上海');
+    const { city, weather } = await reqWeather("上海");
     this.setState({ city, weather });
   };
 
   getTitle = () => {
+    let title = "";
     const path = this.props.location.pathname;
-    // menuList.forEach(item)
+    menuList.forEach(item => {
+      if (item.key === path) {
+        title = item.title;
+      } else if (item.children) {
+        const cItem = item.children.find(cItem => cItem.key === path);
+        if (cItem) {
+          title = cItem.title;
+        }
+      }
+    });
+    return title;
+  };
+
+  getUser = () => {
+    this.user = memoryUtils.user.username;
+  };
+
+  logOut = () => {
+    console.log(4444);
+    memoryUtils.user = {};
+    storageUtils.removeUser();
+    this.props.history.replace("/login");
+    message.success("退出成功");
   };
 
   componentDidMount() {
     this.getCurrentTime();
     this.getWeather();
+    this.getUser();
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
   }
 
   render() {
     const { city, weather, currentTime } = this.state;
 
+    const title = this.getTitle();
+
     return (
       <header>
         <div className='header-top'>
-          <span>欢迎，admin</span>
-          <a href='#!'>退出</a>
+          <span>欢迎，{this.user}</span>
+          <button onClick={this.logOut}>退出</button>
         </div>
         <div className='header-bottom'>
-          <div className='header-bottom-left'>首页</div>
+          <div className='header-bottom-left'>{title}</div>
           <div className='header-bottom-right'>
             <span>{currentTime}</span>
             <img src={weatherPng} alt='晴' />
