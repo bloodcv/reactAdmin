b4efb44fbfb679e77abc912cbdc3323576e7f26f
bloodcv <554365000@qq.com>
2020-05-27 18:05:58 +0800

    [saya]202005271805

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -1,7 +1,31 @@
 import React, { Component } from "react";
+import { Card, Button } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+
+import "../../components/link-button";
+import LinkButton from "../../components/link-button";
 
 export default class Category extends Component {
+  state = {
+    title: "一级品类管理",
+  };
+
   render() {
-    return <div>Category</div>;
+    const { title } = this.state;
+
+    const extra = (
+      <Button type='primary'>
+        <PlusOutlined />
+        添加
+      </Button>
+    );
+
+    return (
+      <Card title={title} extra={extra}>
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
+    );
   }
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 
+import './home.less'
+
 export default class Home extends Component {
   render() {
-    return <div>Home</div>;
+    return (
+      <div className='home-title'>
+        <h1>欢迎使用后台管理系统</h1>
+      </div>
+    );
   }
 }
