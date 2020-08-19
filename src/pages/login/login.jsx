05a99b3a06fbed09363ce50a2cc5fa2b95af3d95
bloodcv <554365000@qq.com>
2020-08-19 17:44:49 +0800

    [saya]202008191744

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -49,6 +49,7 @@ class Login extends Component {
           <h1>React项目：后台管理系统</h1>
         </header>
         <section className='login-content'>
+          <div className={user.errorMsg ? 'error-msg show' : 'error-msg'}>{ user.errorMsg }</div>
           <h2>用户登录</h2>
           <Form
             name='normal_login'
